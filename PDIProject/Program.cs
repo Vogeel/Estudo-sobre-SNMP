7119870c0b441d72c6ebdcf383fc703e84701902
vitor vogel <vitor-vogel@hotmail.com>
2024-04-04 22:02:33 -0300

    feat: processos movidos para o Forms

--- PDIProject/Program.cs
+++ PDIProject/Program.cs
@@ -14,18 +14,20 @@ internal class Program
         string ipEndPoint = "";
         do
         {
-            Console.WriteLine("Bem-vindo ao projeto sobre SNMP! \n Aperte qualquer tecla para fazer busca de ip na rede");
-            Console.ReadKey();
-            GetIpsOnNetwork();
+            Console.WriteLine("Bem-vindo ao projeto sobre SNMP!");
 
 
             if (String.IsNullOrEmpty(ipEndPoint))
             {
+                test();
+                Console.WriteLine("Aperte qualquer tecla para fazer busca de ip na rede");
+                Console.ReadKey();
+                GetIpsOnNetwork();
                 Console.WriteLine("Digite o IP End Point para fazer a busca");
                 ipEndPoint = Console.ReadLine();
             }
 
-            Console.WriteLine("Oque deseja fazer? \n 1- Fazer um walk na rede (Ip pré-definido) \n 2- Buscar o value por um OID especifico \n 0 para SAIR ");
+            Console.WriteLine("Oque deseja fazer? \n 1- Fazer um walk na rede \n 2- Buscar o value por um OID especifico \n 0 para SAIR ");
             options = Console.ReadLine();
 
             switch (options)
@@ -78,8 +80,8 @@ internal class Program
         try
         {
             var oid = new List<Variable> { new Variable(new ObjectIdentifier(oidWrite)) };
-            var result = Messenger.Get(
-                VersionCode.V1,
+             var result = Messenger.Get(
+                VersionCode.V2,
                 new IPEndPoint(IPAddress.Parse(ipEndPoint), 161),
                 new OctetString("public"),
                 oid,
@@ -88,8 +90,8 @@ internal class Program
             if (result != null && result.Count > 0)
             {
                 var value = result[0].Data.ToString();
-
-                Console.WriteLine($"A OID: {oid[0].Id} tem o seu value de: {value} \n");
+                
+                Console.WriteLine($"A OID: {oid[0].Id} tem o seu value de: {value}");
             }
             else
             {
@@ -104,13 +106,16 @@ internal class Program
 
     public static void WalkSNMP(string ipEndPoint)
     {
+        int cont = 0;
+        int oldCont = 0;
+        string res;
         try
         {
             var listResult = new List<Variable>();
             Messenger.Walk(VersionCode.V1,
                                new IPEndPoint(IPAddress.Parse(ipEndPoint), 161),
                                new OctetString("public"),
-                               new ObjectIdentifier("1.3.6.1.2.1.1"),
+                               new ObjectIdentifier("1.3.6.1.2.1"),
                                listResult,
                                60000,
                                WalkMode.WithinSubtree);
@@ -123,14 +128,55 @@ internal class Program
                 sb.AppendLine($" OID: {item.Id.ToString()}");
                 sb.AppendLine($" Value: {item.Data.ToString()}\n");
                 sb.AppendLine("-------\n");
-            }
+                cont++;
+                if(cont == 10)
+                {
+                    string resultString = sb.ToString();
+                    Console.WriteLine(resultString);
 
-            string resultString = sb.ToString();
-            Console.WriteLine(resultString);
+                    oldCont =+ cont;
+                    cont = 0;
+                    Console.WriteLine($"itens listados: {oldCont}, deseja carregar mais? s/n");
+                    res = Console.ReadLine();
+                    if (res == "s")
+                        continue;
+                    else
+                        break;
+                }
+            }
+            
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.ToString());
+        }
+    }
+
+    public static void test()
+    {
+        var ipAddress = IPAddress.Parse("192.168.100.40");
+        var target = new IPEndPoint(ipAddress, 161);
+
+        // Defina a comunidade SNMP
+        var community = new OctetString("public");
+
+        // Defina a OID base para obter informações sobre as interfaces de rede (IF-MIB)
+        var ifMibBase = new ObjectIdentifier("1.3.6.1.2.1.2");
+
+        // Lista para armazenar as informações sobre as interfaces de rede
+        var interfaceInfo = new List<Variable>();
+
+        // Envie uma solicitação SNMP Walk para obter informações sobre as interfaces de rede
+        Messenger.Walk(VersionCode.V2,
+                        target,
+                        community,
+                        ifMibBase,
+                        interfaceInfo,
+                        60000,
+                        WalkMode.WithinSubtree);
+        foreach (Variable variable in interfaceInfo)
+        {
+            Console.WriteLine($"{variable.Id}: {variable.Data}");
         }
     }
 }
---
+++ SnmpSearchForm/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SnmpSearchForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            oidText = new TextBox();
+            label2 = new Label();
+            SearchIpBtn = new Button();
+            label3 = new Label();
+            viewInScreen = new DataGridView();
+            searchBtn = new Button();
+            walkRB = new RadioButton();
+            getRB = new RadioButton();
+            ipText = new TextBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)viewInScreen).BeginInit();
+            SuspendLayout();
+            // 
+            // oidText
+            // 
+            oidText.Location = new Point(155, 56);
+            oidText.Name = "oidText";
+            oidText.Size = new Size(100, 23);
+            oidText.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(155, 38);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 15);
+            label2.TabIndex = 3;
+            label2.Text = "OID";
+            // 
+            // SearchIpBtn
+            // 
+            SearchIpBtn.Location = new Point(585, 56);
+            SearchIpBtn.Name = "SearchIpBtn";
+            SearchIpBtn.Size = new Size(75, 23);
+            SearchIpBtn.TabIndex = 4;
+            SearchIpBtn.Text = "Pesquisar";
+            SearchIpBtn.UseVisualStyleBackColor = true;
+            SearchIpBtn.Click += SearchIpBtn_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(585, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(117, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Pesquisar IPs na rede";
+            // 
+            // viewInScreen
+            // 
+            viewInScreen.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            viewInScreen.Location = new Point(15, 127);
+            viewInScreen.Name = "viewInScreen";
+            viewInScreen.Size = new Size(773, 311);
+            viewInScreen.TabIndex = 7;
+            // 
+            // searchBtn
+            // 
+            searchBtn.Location = new Point(273, 56);
+            searchBtn.Name = "searchBtn";
+            searchBtn.Size = new Size(75, 23);
+            searchBtn.TabIndex = 8;
+            searchBtn.Text = "Pesquisar";
+            searchBtn.UseVisualStyleBackColor = true;
+            searchBtn.Click += searchBtn_Click;
+            // 
+            // walkRB
+            // 
+            walkRB.AutoSize = true;
+            walkRB.Location = new Point(389, 27);
+            walkRB.Name = "walkRB";
+            walkRB.Size = new Size(147, 19);
+            walkRB.TabIndex = 10;
+            walkRB.TabStop = true;
+            walkRB.Text = "Walk com ponto inicial";
+            walkRB.UseVisualStyleBackColor = true;
+            // 
+            // getRB
+            // 
+            getRB.AutoSize = true;
+            getRB.Location = new Point(389, 52);
+            getRB.Name = "getRB";
+            getRB.Size = new Size(92, 19);
+            getRB.TabIndex = 11;
+            getRB.TabStop = true;
+            getRB.Text = "Get com Oid";
+            getRB.UseVisualStyleBackColor = true;
+            // 
+            // ipText
+            // 
+            ipText.Location = new Point(28, 57);
+            ipText.Name = "ipText";
+            ipText.Size = new Size(100, 23);
+            ipText.TabIndex = 12;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(17, 15);
+            label1.TabIndex = 13;
+            label1.Text = "IP";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Controls.Add(ipText);
+            Controls.Add(getRB);
+            Controls.Add(walkRB);
+            Controls.Add(searchBtn);
+            Controls.Add(viewInScreen);
+            Controls.Add(label3);
+            Controls.Add(SearchIpBtn);
+            Controls.Add(label2);
+            Controls.Add(oidText);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)viewInScreen).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox oidText;
+        private Label label2;
+        private Button SearchIpBtn;
+        private Label label3;
+        private DataGridView viewInScreen;
+        private Button searchBtn;
+        private RadioButton walkRB;
+        private RadioButton getRB;
+        private TextBox ipText;
+        private Label label1;
+    }
+}
